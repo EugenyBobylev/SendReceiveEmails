7a692ef64689ee9a73af1e4962cd362cc3576877
Eugeny Bobylev <ebobylev@gmail.com>
2020-01-27 14:42:27 +1000

    Добавлены методы для чтения входящих сообщений из папки INPUT

--- ProxyTest.py
+++ ProxyTest.py
@@ -1,12 +1,48 @@
 from __future__ import print_function
+import base64
+import email
 import pickle
 import os.path
+from apiclient import errors
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def get_message(service, user_id, msg_id):
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+        print('Message snippet: %s' % message['snippet'])
+        return message
+    except errors.HttpError as ex:
+        print(f'An error occurred: \"{ex}\"')
+
+
+def get_mime_message(service, user_id, msg_id):
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id, format='raw').execute()
+        print(f'Message snippet: {message["snippet"]}')
+        msg_str = base64.urlsafe_b64decode(message['raw'].encode('ASCII'))
+        mime_msg = email.message_from_string(msg_str)
+        return mime_msg
+    except errors.HttpError as exc:
+        print(f'An error occurred: {exc}')
+
+
+def get_attachments(service, user_id, msg_id, store_dir):
+    try:
+        message = service.users().messages().get(userId=user_id, id=msg_id).execute()
+        for part in message['payload']['parts']:
+            if part['filename']:
+                file_data = base64.urlsafe_b64decode(part['body']['data'].encode('UTF-8'))
+                path = ''.join([store_dir, part['filename']])
+                f = open(path, 'w')
+                f.write(file_data)
+                f.close()
+    except Exception as exc:
+        print('An error occured: {exc}')
 
 
 def main():
@@ -37,12 +73,21 @@ def main():
     results = service.users().labels().list(userId='me').execute()
     labels = results.get('labels', [])
 
-    if not labels:
-        print('No labels found.')
+    # Query the service object to get User Profile
+    userInfo = service.users().getProfile(userId='me').execute()
+    print("UserInfo is \n %s" % (userInfo))
+
+    # Call the Gmail API to fetch INBOX
+    results = service.users().messages().list(userId='me', labelIds=['INBOX']).execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        print('No messages found.')
     else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
+        print('Messages:')
+        for message in messages:
+            msg = service.users().messages().get(userId='me', id=message['id']).execute()
+            print(msg['snippet'])
 
 
 if __name__ == '__main__':
--- TestSendMail.py
+++ TestSendMail.py
@@ -1,8 +1,8 @@
-from SendEmail import send_mail_ru, get_attach_image, get_plain_mimie, get_attach_mime
+from SendEmail import send_mail_ru, send_gmail_com, get_attach_image, get_plain_mimie, get_attach_mime
 
 if __name__ == '__main__':
-    send_from = "masternz@mail.ru"
-    send_to = ["ebobylev@gmail.com", "beabooks@mail.ru", ]
+    send_from = "ebobylev@gmail.com"
+    send_to = ["beabooks@mail.ru"]  # "masternz@mail.ru",
     subject = "SMTP e-mail тест"
     message = """
 Это электронное письмо было послано самому себе в исключительно отладочных целях.
@@ -10,16 +10,16 @@ if __name__ == '__main__':
 """
 
     msg_root = get_plain_mimie(send_from, send_to, subject, message)
-    msg_attaches = get_attach_mime(["d:\\Книги\\Python\\Шпаргалка по Python.pdf",
-                                    "d:\\Книги\\Python\\info.docx"])
-    msg_images = get_attach_image(["2.jpg"])
+    #msg_attaches = get_attach_mime(["d:\\Книги\\Python\\Шпаргалка по Python.pdf",
+    #                                "d:\\Книги\\Python\\info.docx"])
+    #msg_images = get_attach_image(["2.jpg"])
 
-    for msg_attach in msg_attaches:
-        msg_root.attach(msg_attach)
+    #for msg_attach in msg_attaches:
+    #    msg_root.attach(msg_attach)
 
-    for img in msg_images:
-        msg_root.attach(img)
+    #for img in msg_images:
+    #    msg_root.attach(img)
 
-    send_mail_ru(send_from, send_to, msg_root)
-    # send_gmail_com(send_from, send_to, msg_root)
+    #send_mail_ru(send_from, send_to, msg_root)
+    send_gmail_com(send_from, send_to, msg_root)
     # print(msg_root)
