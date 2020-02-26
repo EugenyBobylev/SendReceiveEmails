e4889b1856cfc52e1a9c0bc14c8ff8c254cb56b5
Bobylev <ebobylev@gmailcom>
2020-02-26 11:37:50 +1000

    доработка get_info, get_api_modes

--- WhatsAppChanel.py
+++ WhatsAppChanel.py
@@ -1,17 +1,30 @@
 import requests
 import json
-from pathlib import Path
 
 
 # chat2desk api info
-def get_info() -> None:
+def get_info() -> object:
     url = "https://api.chat2desk.com/v1/companies/api_info"
     payload = {}
     headers = {'Authorization': '456c1286ccf71bfcd1bda342d92a70'}
 
     response = requests.request("GET", url, headers=headers, data=payload)
-    print(response.text)
+    __data__ = json.load(response) if response.ok else None
+    return __data__
 
+
+# chat2desk api modes
+def get_api_modes() -> object:
+    url = "https://api.chat2desk.com/v1/companies/api_modes"
+    payload = {}
+    headers = {'Authorization': '456c1286ccf71bfcd1bda342d92a70'}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    __data__ = json.load(response) if response.ok else None
+    return __data__
+
+
+# load api info json from file
 def load_data_json():
     __data__ = None
     with open('c:/Users/Bobylev/My Documents/data.json', mode='r', encoding='utf-8') as f:
@@ -19,5 +32,5 @@ def load_data_json():
     return __data__
 
 
-data = load_data_json()
-print(data)
+api_modes = get_api_modes()
+print(api_modes)
