c76dedb20f9da12d0c671590bf29b8e678decba1
Bobylev <ebobylev@gmailcom>
2020-02-26 17:41:46 +1000

    заработала отправка сообщений клиенту по whatsapp

--- WhatsAppChanel.py
+++ WhatsAppChanel.py
@@ -47,8 +47,8 @@ def get_api_channels() -> object:
     return __data__
 
 
-# chat2client api clients (GET)
-def get_api_clients() -> object:
+# chat2client api all clients (GET)
+def get_api_all_clients() -> object:
     url = "https://api.chat2desk.com/v1/clients"
     payload = {}
     headers = {'Authorization': token}
@@ -57,11 +57,34 @@ def get_api_clients() -> object:
     __data__ = json.loads(response.text) if response.ok else None
     return __data__
 
+
+def get_api_clients(client_id) -> object:
+    url = f"https://api.chat2desk.com/v1/clients/{client_id}"
+    payload = {}
+    headers = {'Authorization': token}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    __data__ = json.loads(response.text) if response.ok else None
+    return __data__
+
+
+def get_api_clients_phone(phone) -> object:
+    url = f"https://api.chat2desk.com/v1/clients?phone={phone}"
+    payload = {}
+    headers = {'Authorization': token}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+    __data__ = json.loads(response.text) if response.ok else None
+    return __data__
+
+
 # chat2desk api create new client (POST)
-def post_api_client() -> object:
+def post_api_client(phone, nick) -> object:
     url = "https://api.chat2desk.com/v1/clients"
 
-    payload = '{\n\t"phone":79247401790,\n\t"transport":"viber_public"\n\t,"nickname":"EugenyBobylev"\n}'
+    # payload = '{\n\t"phone":79247401790,\n\t"transport":"viber_public"\n\t,"nickname":"EugenyBobylev"\n}'
+    body = {"phone": phone, "transport":"viber_public", "nickname": nick}
+    payload = json.dumps(body)
     headers = {
         'Authorization': token,
         'Content-Type': 'application/json',
@@ -89,6 +112,23 @@ def put_api_clients(client_id) -> object:
     return __data__
 
 
+# chat2desk api send message
+def post_api_message(client_id, message) -> object:
+    url = "https://api.chat2desk.com/v1/messages"
+
+    body = {"client_id": client_id, "text": message, "type": "to_client", "chanel_id": 19286, "transport": "whatsapp"}
+    payload = json.dumps(body)
+    headers = {
+        'Authorization': token,
+        'Content-Type': 'application/json',
+        'charset': 'UTF=8'
+    }
+
+    response = requests.request("POST", url, headers=headers, data=payload.encode('utf-8'))
+    __data__ = json.loads(response.text)
+    return __data__
+
+
 # load api info json from file
 def load_data_json():
     __data__ = None
@@ -99,9 +139,13 @@ def load_data_json():
 
 data = None
 # data = get_api_modes()
-#data = get_api_transports()
-#data = get_api_clients()
+# data = get_api_transports()
+# data = get_api_all_clients()
 # data = get_api_channels()
-# data = post_api_client()
+# data = post_api_client(phone=79246432292 ,nick='OlgaOh')
 # data = put_api_clients(client_id=96881373)
+# data = get_api_clients(client_id=105582161)
+# data = get_api_clients_phone(79247401790)
+data = post_api_message(client_id=105582161, message='Здравствуйте Ольга Владимировна!')
+# chanel_id = 19286
 print(data)
